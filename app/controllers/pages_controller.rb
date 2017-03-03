58cc3f86c4dcbe4be6207fabb90958420414384c
Jim Padilla <jp_code2013@yahoo.com>
2017-03-03 09:21:07 -0800

    Add routes, controllers for about, projects, admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
-
+  end
+  def about
+  end
+  def projects
+  end
+  def admin
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'projects', to: 'pages#projects', as: 'projects'
+  get 'admin', to: 'pages#admin', as: 'admin'
 end
